5e75de879021f739205aa405cd46d01c6785a876
BrianSerem <brianseremiat@gmail.com>
2021-09-21 23:01:10 +0300

    tried and tested for now nsvsPlease enter the comqQ!mit message for your changes. Lines starting ~~``
    `` branch is up to date with 'origin/develop'.

--- database/connection.js
+++ database/connection.js
@@ -15,4 +15,19 @@ const closeDbConnection = () => {
     return mongoose.disconnect()
 };
 
+function connectToDbAgain() {
+    mongoose.connect(process.env.LOCAL_DB, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }, (err) => {
+        if (err) throw err;
+        console.log('database connection made!!');
+    })
+};
+
+const closeDbConnection = () => {
+    console.log('connection closed')
+    return mongoose.disconnect()
+};
+
 module.exports = {connectToDb, closeDbConnection};
